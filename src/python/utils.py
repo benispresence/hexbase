96f4d29f202d5f8ff4e76dd1699101e15b5921b5
benjaminharmat <benjaminharmat@gmail.com>
2022-04-20 17:56:14 +0200

    Adjust repo structure, make more modular

---
+++ src/python/utils.py
@@ -0,0 +1,20 @@
+from python.configs.config import get_pg_conn
+
+pg_conn = get_pg_conn()
+
+
+def get_last_block_number_from_b_dwh(schema, table, column):
+    """
+    :param schema: B-DWH Schema name (str)
+    :param table: B-DWH table name (str)
+    :param column: B-DWH block_number column name (str)
+    :return: Returns the last block number from one of the tables from the (B-DWH) Blockchain Data Warehouse.
+    """
+    last_block_query = f'SELECT max({column} :: INT) FROM {schema}.{table};'
+
+    with pg_conn:
+        with pg_conn.cursor() as curs:
+            curs.execute(last_block_query)
+            block_num = curs.fetchall()
+
+    return block_num[0][0]
