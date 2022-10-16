d535d9f3c13564fbf117b1565766607852dc4fbf
benjaminharmat <benjaminharmat@gmail.com>
2022-10-16 14:58:30 +0200

    change to database v3 config

--- configs/postgres.py
+++ configs/postgres.py
@@ -6,7 +6,7 @@ from configs.credentials import get_config
 def get_pg_conn():
     config = get_config()
     pg_conn = psycopg2.connect(
-        dbname=config['postgre_db']['dbname_hexbase_v2']
+        dbname=config['postgre_db']['dbname_hexbase_v3']
         , user=config['postgre_db']['user']
         , password=config['postgre_db']['password']
         , host=config['postgre_db']['host']
