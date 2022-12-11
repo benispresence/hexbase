347dfaa240153b59b9223c1f38325449868d0fa3
benjaminharmat <benjaminharmat@gmail.com>
2022-12-11 11:33:53 +0100

    remove not used infura connection

--- configs/web3.py
+++ configs/web3.py
@@ -1,17 +1,5 @@
 from web3 import Web3
 
-from configs.credentials import get_config
-
-
-def get_infura_conn():
-    """
-    :return: A connection to web3 via the infura api service.
-    """
-    config = get_config()
-    infura_url = "https://mainnet.infura.io/v3/"+config['infura']['token']
-    web3_conn = Web3(Web3.HTTPProvider(infura_url))
-    return web3_conn
-
 
 def get_local_node_conn():
     """
