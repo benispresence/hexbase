32e2b34a3fc466122d737d996b71f56e902d785c
benjaminharmat <benjaminharmat@gmail.com>
2022-12-17 08:16:27 +0100

    change static localhost rpc url to configurable

--- configs/web3.py
+++ configs/web3.py
@@ -1,10 +1,13 @@
 from web3 import Web3
 
+from configs.credentials import get_config
+
 
 def get_local_node_conn():
     """
     :return: A connection to web3 via a local ethereum node.
     """
-    local_node_url = "http://localhost:8545"
-    web3_conn = Web3(Web3.HTTPProvider(local_node_url))
+    config = get_config()
+    url = config['rpc']['url']
+    web3_conn = Web3(Web3.HTTPProvider(url))
     return web3_conn
